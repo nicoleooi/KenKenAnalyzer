87ef11cd90c7c96474755572edbb1dddd5dea3fa
Lukas Bauer <lukas.bauer.j@gmail.com>
2020-10-28 19:39:45 -0400

    Defined board configuration

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 
 from nnf import Var
 from lib204 import Encoding
+from string import ascii_lowercase
 
 # Call your variables whatever you want
 a = Var('a')
@@ -9,6 +10,46 @@ c = Var('c')
 x = Var('x')
 y = Var('y')
 z = Var('z')
+
+row = []
+col = []
+
+
+# make row and column lists
+for i in range(5):
+    row.append(Var(f'row_{i}'))
+
+for i in range(5):
+    col.append(Var(f'col_{i}'))
+print(row)
+print(col)
+
+
+# create squares a0-e5 as nnf Variables
+squares = []
+for i in range(5):
+    for j in range(5):
+        charOffset = chr(ord('a')+i)
+        squares.append(Var(f'{charOffset}{j}'))
+
+# should find a way to categorize the squares into regions automatically given a config text file ... in the meantime we will do it manually
+o = []
+o.append([[squares[0],squares[1],squares[5],squares[6]],14,'+'])
+o.append([[squares[2],squares[7]],3,'-'])
+o.append([[squares[3], squares[4]],4,'/'])
+o.append([[squares[8],squares[9]],2,'-'])
+o.append([[squares[10],squares[11]],2,'-'])
+o.append([[squares[12],squares[17],squares[18],squares[23]],40,'*'])
+o.append([[squares[13],squares[14],squares[19]],9,'+'])
+o.append([[squares[15],squares[20]],2,'/'])
+o.append([[squares[16],squares[21],squares[22]],12,"*"])
+o.append([[squares[24]],5,'!'])
+
+print("\nInitial board configuration:")
+for item in o:
+    print(item)
+print("* Note: ! indicates singular value in the specified box")
+
 
 
 #
@@ -24,11 +65,17 @@ def example_theory():
     E.add_constraint(c | y | z)
     return E
 
+def test_kenken():
+    #row[i] << - >> (the row i contains the digits 1-5)
+    #col[i] << - >> (the col i contains the digits 1-5)
+    return
+
+
 
 if __name__ == "__main__":
 
     T = example_theory()
-
+    print("Just testing the example theory. Ignore below.")
     print("\nSatisfiable: %s" % T.is_satisfiable())
     print("# Solutions: %d" % T.count_solutions())
     print("   Solution: %s" % T.solve())
